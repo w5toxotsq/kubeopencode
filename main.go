0d84d9b6bcde8e721cc142180cf50aa7556aae16
Walt Stokes <w5toxotsq@users.noreply.github.com>
2026-04-15 22:54:20 +0800

    print error message to stderr before exiting on failure

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/kubeopencode/kubeopencode/cmd"
@@ -10,6 +11,7 @@ import (
 // It delegates execution to the root cobra command defined in cmd/root.go.
 func main() {
 	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
